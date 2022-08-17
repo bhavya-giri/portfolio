2e934dbe37b35d035d80985435b4e5b55bb622cf
Bhavya Giri <bhavyagiri@Bhavyas-MacBook-Pro.local>
2022-08-17 18:26:50 +0530

    minner

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Setup = () => {
   return (
     <div className='absolute w-1/2 h-full right-10'>
-        <h1 className=' text-3xl pt-11 text-center font-bold '>My Setup</h1>
+        <h1 className=' text-3xl pt-11 text-center text-gray-700'>My Setup</h1>
         <iframe className='bottom-3 pt-10np' src='https://my.spline.design/miniroomartcopy-9ae3537ffaa5bbad3eb138808c3111c3/' frameborder='0' width='100%' height='60%'></iframe>
     
     </div>
