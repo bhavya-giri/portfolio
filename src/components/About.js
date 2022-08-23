f3f2154dfed1243a61c0c878f3111cb24f815253
Bhavya Giri <bhavya.giri44@gmail.com>
2022-08-23 17:16:56 +0530

    project almost done

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ const About = () => {
     <div className="pb-36">
       <h1 className="text-6xl ml-16 md:ml-32">/ about</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 ">
-        <div className="text-xl order-2 md:order-1 ml-16 md:ml-32 mt-8">
+        <div className="text-xl order-2 md:order-1 mx-5 md:ml-32 mt-8">
           <div className=" ">
             I am front-end developer and open source entusiast. I building my
             skill to become full-stack developer, exploring horzions of cloud
@@ -32,7 +32,7 @@ const About = () => {
           
         </div>
         <img
-          className="max-w-xs mx-auto order-1 pt-6 md:order-2 hover:translate-y-2.5	"
+          className="max-w-xs mx-auto order-1 pt-10 md:order-2 hover:translate-y-2.5	"
           src="https://pbs.twimg.com/profile_images/1559744099099353089/yb1q2J5I_400x400.jpg"
           alt=""
         />
